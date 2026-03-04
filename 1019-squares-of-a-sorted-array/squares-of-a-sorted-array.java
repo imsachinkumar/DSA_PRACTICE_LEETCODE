ba9b45ed3f5560f4f6d4613bd04b1907d9c90d69
imsachinkumar <122716893+imsachinkumar@users.noreply.github.com>
2026-03-04 19:38:36 +0530

    Time: 1 ms (99.87%) | Memory: 48.4 MB (15.67%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
@@ -1,49 +1,47 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-
-        int[] neg = new int[nums.length];
-        int[] pos = new int[nums.length];
-
-        int n = 0;
-        int p = 0;
-
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] <= 0){
-                neg[n++] = nums[i] * nums[i];
-            } else {
-                pos[p++] = nums[i] * nums[i];
-            }
+      
+      int[]neg=new int[nums.length];
+      int n=0;
+      int[]pos=new int[nums.length];
+      int p=0;
+      for(int i=0;i<nums.length;i++){
+        if(nums[i]<=0){
+            neg[n++]=nums[i]*nums[i];
+        }else{
+            pos[p++]=nums[i]*nums[i];
         }
+      }
+      int i=0;
+      int j=0;
+      int idx=0;
+      int[]merge_array=new int[nums.length];
 
-        // reverse negative squares
-        int l = 0, r = n - 1;
-        while(l < r){
-            int temp = neg[l];
-            neg[l] = neg[r];
-            neg[r] = temp;
-            l++;
-            r--;
+      int l=0;
+      int m=n-1;
+
+      while(l<m){
+        int temp=neg[l];
+        neg[l]=neg[m];
+        neg[m]=temp;
+        l++;
+        m--;
+
+      }
+
+      while(i<n && j<p){
+        if(neg[i]<=pos[j]){
+            merge_array[idx++]=neg[i++];
+        }else{
+        merge_array[idx++]=pos[j++];
         }
-
-        int i = 0, j = 0, idx = 0;
-        int[] merge_array = new int[nums.length];
-
-        while(i < n && j < p){
-            if(neg[i] <= pos[j]){
-                merge_array[idx++] = neg[i++];
-            } else {
-                merge_array[idx++] = pos[j++];
-            }
-        }
-
-        while(i < n){
-            merge_array[idx++] = neg[i++];
-        }
-
-        while(j < p){
-            merge_array[idx++] = pos[j++];
-        }
-
-        return merge_array;
+      }
+      while(i<n){
+        merge_array[idx++]=neg[i++];
+      }
+      while(j<p){
+        merge_array[idx++]=pos[j++];
+      }
+      return merge_array;
     }
 }
